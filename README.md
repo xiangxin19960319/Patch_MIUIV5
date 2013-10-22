49b9089cc075f292313e7a3cf0654bdf4af6d1a0
xiangxin19960319 <xiangxin19960319@gmail.com>
2013-10-22 17:50:11 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+
+.....build
+
+mkdir patchrom-miui-v5
+
+git clone https://github.com/xiangxin19960319/Patch-Miui-V5_kumquat patchrom-miui-v5
+
+cd patchrom-miui-v5
+
+./build/envsetup.sh
+
+cd st25i
+
+make fullota
+
+cp out/fullota.zip .
---
+++ build/jellybean42.mk
@@ -0,0 +1,65 @@
+private-miui-apps := AntiSpam  \
+					 Backup  \
+					 BaiduNetworkLocation  \
+					 Browser  \
+					 BugReport  \
+					 Calculator  \
+					 Calendar  \
+					 CalendarProvider  \
+					 CloudService  \
+					 Contacts  \
+					 ContactsProvider  \
+					 DataHubProvider \
+					 DeskClock  \
+					 DownloadProvider  \
+					 DownloadProviderUi  \
+					 Email  \
+					 FileExplorer  \
+					 FusedLocation \
+					 LBESEC_MIUI \
+					 MiuiGallery  \
+					 MediaProvider  \
+					 MiuiCompass  \
+					 MiuiHome  \
+					 MiuiSystemUI  \
+					 Mms  \
+					 Music  \
+					 Notes  \
+					 PackageInstaller  \
+					 Phone  \
+					 Provision \
+					 QuickSearchBox  \
+					 Settings  \
+					 SettingsProvider  \
+					 SoundRecorder  \
+					 SuperMarket  \
+					 TelephonyProvider  \
+					 ThemeManager  \
+					 Updater \
+					 Weather \
+					 WeatherProvider \
+					 GuardProvider \
+					 NetworkAssistant \
+					 XiaomiServiceFramework \
+					 MiuiVideoPlayer \
+					 VpnDialogs \
+					 YellowPage \
+					 Exchange2 \
+					 AirkanPhoneService \
+					 Transfer \
+					 MiWallpaper
+
+private-preinstall-apps := VoiceAssist  \
+					 GameCenter \
+					 GameCenterSDKService \
+					 InputMethod \
+					 AlipayMsp \
+					 MiuiVideo \
+					 MiShop \
+					 Userbook
+
+private-miui-jars := android.policy \
+					 framework \
+					 framework_ext \
+					 telephony-common \
+					 services
